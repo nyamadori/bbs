2d6eb2ef4c62f439148906ce0392799ff2913f0d
nyamadori <nyamadorig@gmail.com>
2016-08-11 17:00:57 +0900

    Revert migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811071757) do
-
-  create_table "messages", force: :cascade do |t|
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end

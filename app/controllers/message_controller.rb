fb59f2e42575de3d2a6b7faf56509310a0cf911f
nyamadori <nyamadorig@gmail.com>
2016-08-11 16:42:07 +0900

    Add actions

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,2 +1,21 @@
 class MessageController < ApplicationController
+  def index
+    # TODO
+    # メッセージ一覧取得 (JSON)
+  end
+
+  def create
+    # TODO
+    # メッセージ投稿
+  end
+
+  def destroy
+    # TODO
+    # メッセージ削除
+  end
+
+  def update
+    # TODO
+    # メッセージ編集
+  end
 end
